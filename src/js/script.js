8949dcd7f08a52fe01707af311843ae93f308aff
Lauritsen1 <43439056+Lauritsen1@users.noreply.github.com>
2020-09-20 23:57:24 +0200

    restart game function
    You can now restart the game after you've won

--- src/js/script.js
+++ src/js/script.js
@@ -1,33 +1,5 @@
-let cardIds = [];
-
-// Generates parring id's
-for (let i = 0; i < 8; i++) {
-    cardIds.push(i + 1);
-    cardIds.push(i + 1);
-}
-
-// Shuffles array
-function shuffle(a) {
-    for (let i = a.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [a[i], a[j]] = [a[j], a[i]];
-    }
-    return a;
-}
-
-shuffle(cardIds);
-
 // Array with all cards
 let cards = document.querySelectorAll('.card');
-
-let cardImages = document.querySelectorAll('.card-front img');
-
-cards.forEach((card, index) => {
-    // Gives each card an id
-    card.dataset.id = cardIds[index];
-
-    cardImages[index].src = `/assets/images/${cardIds[index]}.jpg`
-});
 
 // Adds click event to all cards
 function addClickEvent() {
@@ -37,7 +9,41 @@ function addClickEvent() {
     });
 }
 
-addClickEvent();
+function startGame() {
+
+    let cardIds = [];
+
+    // Generates parring id's
+    for (let i = 0; i < 8; i++) {
+        cardIds.push(i + 1);
+        cardIds.push(i + 1);
+    }
+
+    // Shuffles array
+    function shuffle(a) {
+        for (let i = a.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [a[i], a[j]] = [a[j], a[i]];
+        }
+        return a;
+    }
+
+    shuffle(cardIds);
+
+    let cardImages = document.querySelectorAll('.card-front img');
+
+    cards.forEach((card, index) => {
+        // Gives each card an id
+        card.dataset.id = cardIds[index];
+
+        //  Sets an image on each card with the same id
+        cardImages[index].src = `/assets/images/${cardIds[index]}.jpg`
+    });
+
+    addClickEvent();
+}
+
+startGame();
 
 // Array with id's of active cards
 let activeCardIds = [];
@@ -90,6 +96,30 @@ function gameSession() {
     let pairredCards = document.querySelectorAll('.card--pairred');
 
     if (pairredCards.length == cards.length) {
-        alert('YOU WON!');
+        setTimeout(() => {
+            restartGame();
+        }, 1000);
     }
+}
+
+function restartGame() {
+
+    let modal = document.querySelector('.modal');
+    modal.style.display = 'flex';
+
+    let restartBtn = document.querySelector('.modal__restart-btn');
+
+    restartBtn.addEventListener('click', () => {
+        modal.style.display = 'none';
+
+        // Array with all cards
+        let cardsInner = document.querySelectorAll('.card-inner');
+
+        cardsInner.forEach(cardInner => {
+            cardInner.classList.remove('card--active');
+            cardInner.classList.replace('card--pairred', 'card--notpairred');
+        });
+
+        startGame();
+    });
 }
